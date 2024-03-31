cd547a537cd084d7754da6f4fee97ed133980085
GuillaumeRhodes <rhodes.guillaume46@gmail.com>
2024-03-31 20:26:09 +0200

    image entity created

---
+++ versus-project-back/src/Entity/Image.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $url = null;
+
+    #[ORM\Column]
+    private ?int $vote_count = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getVoteCount(): ?int
+    {
+        return $this->vote_count;
+    }
+
+    public function setVoteCount(int $vote_count): static
+    {
+        $this->vote_count = $vote_count;
+
+        return $this;
+    }
+}
